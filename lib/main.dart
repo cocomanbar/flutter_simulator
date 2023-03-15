53c993055d7c2a8eb8eec6af77e445f8b5ff1d07
cocomanbar <125322078@qq.com>
2023-03-15 23:23:09 +0800

    report

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simulator/Utils/fstoast.dart';
 import 'package:flutter_simulator/route/fsroute.dart';
 import 'package:flutter_simulator/launch/launch_page.dart';
 import 'package:flutter_simulator/system_info/system_info.dart';
+import 'dart:async';
+
+// 上报错误和日志逻辑
+void reportErrorDetails(FlutterErrorDetails details) {
+  print("details ===>>> $details");
+}
+
+// 构建错误信息
+FlutterErrorDetails makeDetails(Object obj, StackTrace stack) {
+  return FlutterErrorDetails(exception: obj, stack: stack);
+}
 
 void main() {
-  runApp(const MyApp());
+  // Flutter framework 异常捕获
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FSToast.showToast("msg3");
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode) {
+      Zone.current.handleUncaughtError(
+          details.exception, details.stack ?? StackTrace.current);
+    }
+  };
+
+  // 其他类型的捕获
+  runZoned(
+    () => runApp(const MyApp()),
+    // 1.收集print打印日志
+    zoneSpecification: ZoneSpecification(
+      // 拦截未处理的异步错误
+      handleUncaughtError: (self, parent, zone, error, stackTrace) {
+        FlutterErrorDetails details = makeDetails(error, stackTrace);
+        reportErrorDetails(details);
+      },
+    ),
+  );
 }
 
 // dev env
